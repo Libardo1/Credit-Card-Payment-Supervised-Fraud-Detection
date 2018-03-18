db1d4c14977d3e9ee8a5ff94be2db23a9578c5e0
CHONG LI <lichong@marshall.usc.edu>
2018-03-18 14:44:10 -0700

    Update Type I Variable.R

--- Building Variable/Type I Variable.R
+++ Building Variable/Type I Variable.R
@@ -1,161 +1,144 @@
 load("Card Payments_Cleaned.rda")
 
-library(data.table)
 library(dplyr)
 
-data = as.data.table(data)
+
+#############################  Type I Variable     ######################
+# historical count with 3 days time window
+
+##############   Subset trial     #############
+data_Jan = filter(data, date < "2010-02-01")
+current = data_Jan[6748,]
+row = 6748
+
+###### time window subsetting method
+ptm <- proc.time()
+#subset = filter(data_Jan, date >= current$date - 3, recordnum < row)
+for (i in 1:nrow(data_Jan)) {
+    current_date = data_Jan[i,"date"]
+    current_key = data_Jan[i,"cardnum"]
+    subset = data_Jan %>% 
+        filter(date >= current_date - 3, recordnum < i, cardnum == current_key) %>%
+        select(cardnum, amount)
+}
+proc.time() - ptm
+# 20s
+
+ptm <- proc.time()
+#subset = data_Jan[data_Jan$date >= current$date - 3 & data_Jan$recordnum < row,]
+for (i in 1:nrow(data_Jan)) {
+    current_date = data_Jan[i,"date"]
+    current_key = data_Jan[i,"cardnum"]
+    subset = data_Jan[data_Jan$date >= current_date - 3 & data_Jan$recordnum < i & data_Jan$cardnum == current_key, c("cardnum", "amount")]
+}
+proc.time() - ptm
+# 11s
 
 
-############
-#Transaction Frequency:
+###### 3-day trial on the subset
+ptm <- proc.time()
+for (i in 1:nrow(data_Jan)) {
+    current_date = data_Jan[i,"date"]
+    current_key = data_Jan[i,"cardnum"]
+    current_amount = data_Jan[i,"amount"]
+    subset = data_Jan[data_Jan$date >= current_date - 3 & data_Jan$recordnum < i & data_Jan$cardnum == current_key, c("cardnum", "amount")]
+    if (nrow(subset) != 0) {
+        avg = mean(subset$amount)
+        max = max(subset$amount)
+        median = median(subset$amount)
+        total = sum(subset$amount)
+        data_Jan[i, paste0("amount_hist_avg_","3")] = current_amount / avg
+        data_Jan[i, paste0("amount_hist_max_","3")] = current_amount / max
+        data_Jan[i, paste0("amount_hist_median_","3")] = current_amount / median
+        data_Jan[i, paste0("amount_hist_total_","3")] = current_amount / total
+    } else {
+        data_Jan[i, paste0("amount_hist_avg_","3")] = 1
+        data_Jan[i, paste0("amount_hist_max_","3")] = 0
+        data_Jan[i, paste0("amount_hist_median_","3")] = 1
+        data_Jan[i, paste0("amount_hist_total_","3")] = 0
+    }
+}
+proc.time() - ptm
+# 15s
 
-lagcum = function(dt,n){
-  dt1 = dt
-  dt[,join_date:=(date-n)]
-  dt1$join_date = dt1$date
-  key1 = colnames(dt)[1]
-  key2 = colnames(dt)[5]
-  setkeyv(dt,c(key1,key2))
-  setkeyv(dt1,c(key1,key2))
-  dt2 = dt1[dt,roll=T,rollends=c(T,T)]
-  return (dt2)
+##############   Function packaging     #############
+build_var <- function(df, time_window, key) {
+    
+    ###########################
+    # df: the name of the cleaned data frame 
+    # time_window: 3 or 7 or other
+    # key: "card" or "merchant"
+    ###########################
+    
+    df[, paste0(key, "_", "amount_to_avg_", time_window)] = 1
+    df[, paste0(key, "_", "amount_to_max_", time_window)] = 0
+    df[, paste0(key, "_", "amount_to_median_", time_window)] = 1
+    df[, paste0(key, "_", "amount_to_total_", time_window)] = 0
+    for (i in 1:nrow(df)) {
+        #print(i)
+        current_date = df[i,"date"]
+        current_amount = df[i,"amount"]
+        if (key == "card") {
+            current_key = df[i,"cardnum"]
+            subset = df[df$date >= current_date - time_window & df$recordnum < i & df$cardnum == current_key, c("cardnum", "amount")]
+        } else if (key == "merchant") {
+            current_key = df[i,"merchnum"]
+            subset = df[df$date >= current_date - time_window & df$recordnum < i & df$merchnum == current_key, c("merchnum", "amount")]
+        }
+        #print(nrow(subset))
+        if (nrow(subset) != 0) {
+            avg = mean(subset$amount)
+            max = max(subset$amount)
+            median = median(subset$amount)
+            total = sum(subset$amount)
+            df[i, paste0(key, "_", "amount_to_avg_",time_window)] = current_amount / avg
+            df[i, paste0(key, "_", "amount_to_max_", time_window)] = current_amount / max
+            df[i, paste0(key, "_", "amount_to_median_", time_window)] = current_amount / median
+            df[i, paste0(key, "_", "amount_to_total_", time_window)] = current_amount / total
+        }
+    }
+    return(df)
 }
 
-cname = function(name) c('date', paste0(name,'Daily'), paste0(name,'Cum'))
+# ptm <- proc.time()
+# data_Jan = build_var(data_Jan, 3, "card")
+# proc.time() - ptm
+# # 14s
+# 
+# ptm <- proc.time()
+# data_Jan = build_var(data_Jan, 7, "card")
+# proc.time() - ptm
+# # 14s
+# 
+# ptm <- proc.time()
+# data_Jan = build_var(data_Jan, 3, "merchant")
+# proc.time() - ptm
+# # 5s
+# 
+# ptm <- proc.time()
+# data_Jan = build_var(data_Jan, 7, "merchant")
+# proc.time() - ptm
+# # 5s
 
-##requency of transaction - merchant 
-daily_merch = data[,.(daily_merch=.N),by=.(date,merchnum)]
-daily_merch=daily_merch[,.(date,daily_merch,cum_merch=cumsum(daily_merch)),by=merchnum]
+##############   Run on the whole dataset     #############
+ptm <- proc.time()
+data = build_var(data, 3, "card")
+proc.time() - ptm
+# 1016 s = 17 min
 
-merch = lagcum(daily_merch,3)
-merch$merchLag3Cum = ifelse(merch$date>merch$join_date,merch$i.cum_merch,merch$i.cum_merch-merch$cum_merch)
-merch = merch[,c(1,6,7,8,9)]
-colnames(merch)[2:4]=cname('merch')
+ptm <- proc.time()
+data = build_var(data, 3, "merchant")
+proc.time() - ptm
+# 774 s = 13 min
 
-merch2 = lagcum(daily_merch,7)
-merch$merchLag7Cum = ifelse(merch2$date>merch2$join_date,merch2$i.cum_merch,merch2$i.cum_merch-merch2$cum_merch)
+ptm <- proc.time()
+data = build_var(data, 7, "card")
+proc.time() - ptm
+# 1265 s = 21 min
 
-merch2 = lagcum(daily_merch,14)
-merch$merchLag14Cum = ifelse(merch2$date>merch2$join_date,merch2$i.cum_merch,merch2$i.cum_merch-merch2$cum_merch)
+ptm <- proc.time()
+data = build_var(data, 7, "merchant")
+proc.time() - ptm
+# 866 s = 14 min
 
-merch2 = lagcum(daily_merch,21)
-merch$merchLag21Cum = ifelse(merch2$date>merch2$join_date,merch2$i.cum_merch,merch2$i.cum_merch-merch2$cum_merch)
-
-##Frequency of transaction - card number
-
-daily_card = data[,.(daily_card=.N),by=.(date,cardnum)]
-daily_card=daily_card[,.(date,daily_card,cum_card=cumsum(daily_card)),by=cardnum]
-
-card = lagcum(daily_card,3)
-card$cardLag3Cum = ifelse(card$date>card$join_date,card$i.cum_card,card$i.cum_card-card$cum_card)
-card = card[,c(1,6,7,8,9)]
-colnames(card)[2:4]=cname('card')
-
-card2 = lagcum(daily_card,7)
-card$cardLag7Cum = ifelse(card2$date>card2$join_date,card2$i.cum_card,card2$i.cum_card-card2$cum_card)
-
-card2 = lagcum(daily_card,14)
-card$cardLag14Cum = ifelse(card2$date>card2$join_date,card2$i.cum_card,card2$i.cum_card-card2$cum_card)
-
-card2 = lagcum(daily_card,21)
-card$cardLag21Cum = ifelse(card2$date>card2$join_date,card2$i.cum_card,card2$i.cum_card-card2$cum_card)
-
-##########
-rm(merch2)
-rm(daily_merch)
-
-rm(card2)
-rm(daily_card)
-##########
-
-data = left_join(data,merch[,-4],by=c('date'='date','merchnum'='merchnum'))
-data = left_join(data,card[,-4],by=c('date'='date','cardnum'='cardnum'))
-
-################################################################################################
-#Location: 
- # Merchan # with Zip 
-
-laguni1 = function(dt,n){
-  dt1 = dt
-  dt[,join_date:=(date-n)]
-  dt1$join_date = dt1$date
-  key1 = colnames(dt)[3]
-  key2 = colnames(dt)[5]
-  setkeyv(dt,c(key1,key2))
-  setkeyv(dt1,c(key1,key2))
-  dt2 = dt1[dt,mult='last',roll=T,rollends=c(T,T)]
-  return (dt2)
-}
-
-
-# count of unique zips related to one merchnum
-zip_merch0 = data[,.(date,merch.zip,merchnum)]
-zip_merch0 = zip_merch0[,uni_zip_merch:=cumsum(!duplicated(merch.zip)),by=merchnum]
-
-zip_merch = laguni1(zip_merch0,3)
-zip_merch$zipLag3merch = ifelse(zip_merch$date>zip_merch$join_date,
-                                zip_merch$i.uni_zip_merch,
-                                zip_merch$i.uni_zip_merch-zip_merch$uni_zip_merch+1)
-zip_merch = zip_merch[,c(3,6,7,9)]
-colnames(zip_merch)[2:3] = substring(colnames(zip_merch)[2:3],3)
-
-zip_merch1 = laguni1(zip_merch0,7)
-zip_merch$zipLag7merch = ifelse(zip_merch1$date>zip_merch1$join_date,
-                                zip_merch1$i.uni_zip_merch,
-                                zip_merch1$i.uni_zip_merch-zip_merch1$uni_zip_merch+1)
-
-zip_merch1 = laguni1(zip_merch0,14)
-zip_merch$zipLag14merch = ifelse(zip_merch1$date>zip_merch1$join_date,
-                                zip_merch1$i.uni_zip_merch,
-                                zip_merch1$i.uni_zip_merch-zip_merch1$uni_zip_merch+1)
-
-zip_merch1 = laguni1(zip_merch0,21)
-zip_merch$zipLag21merch = ifelse(zip_merch1$date>zip_merch1$join_date,
-                                 zip_merch1$i.uni_zip_merch,
-                                 zip_merch1$i.uni_zip_merch-zip_merch1$uni_zip_merch+1)
-
-zip_merch = unique(zip_merch)
-
-data1 = left_join(data,zip_merch,
-                 by=c('date'='date', 'merchnum'='merchnum', 'merch.zip'='merch.zip'))
-
-###Something's wrong!!!!!
-
-#a = data[duplicated(data$recordnum), ]
-#a = zip_merch[duplicated(zip_merch, by=c("date", "merchnum", "merch.zip")), ]
-#b = data[data$recordnum %in% a$recordnum, ]
-#a = data["5090345469809", on = .(merchnum)]
-################################################################################################
-#ransaction Amount - merchant & card number (total of 10):
-#Amount / historical average 
-lagcum = function(dt,n){
-  dt1 = dt
-  dt[,join_date:=(date-n)]
-  dt1$join_date = dt1$date
-  key1 = colnames(dt)[1]
-  key2 = colnames(dt)[5]
-  setkeyv(dt,c(key1,key2))
-  setkeyv(dt1,c(key1,key2))
-  dt2 = dt1[dt,roll=T,rollends=c(T,T)]
-  return (dt2)
-}
-
-
-daily_amt = data[,.(daily_amount=.N),by=.(date,merchnum)]
-daily_amt=daily_amt[,.(date,daily_amount,cum_amount=cumsum(daily_amount)),by=merchnum]
-
-merch = lagcum(daily_merch,3)
-merch$merchLag3Cum = ifelse(merch$date>merch$join_date,merch$i.cum_merch,merch$i.cum_merch-merch$cum_merch)
-merch = merch[,c(1,6,7,8,9)]
-colnames(merch)[2:4]=cname('merch')
-
-
-
-
-
-
-
-#Amount / historical max 
-#Amount / historical median - best yet
-#Current Cumulative total / historical cumulative total 
-#Current amount / historical cumulative total 
+save(data, file = "data_type_a.Rdata")
